2841d6730053234115b5ccec73e354954d1d44c0
r2r90 <aghartur@gmail.com>
2023-03-24 17:09:48 +0100

    SECRET PASSWORD FOR DB

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ import express from "express";
 const app = express();
 import tasks from "./routes/tasks.js";
 import connectDB from "./db/connect.js";
+import dotenv from 'dotenv'
 
 //middleware
 app.use(express.json());
-
+dotenv.config()
 //routes
 
 app.get("/hello", (req, res) => {
@@ -14,17 +15,12 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-// app.get('/api/v1/tasks')      -get all the tasks
-// app.post('/api/v1/tasks')     -create a new task
-// app.get('/api/v1/tasks/:id')  -get single tasks
-// app.patch('/api/v1/tasks/:id')    -update task
-// app.delete('/api/v1/tasks'/:id)   -delete task
 
 const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on ${port}...`));
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
-const connectionString =
-  "mongodb+srv://root:root@taskmanagerproject.pyo7uwn.mongodb.net/task-manager?retryWrites=true&w=majority";
+  
 
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
